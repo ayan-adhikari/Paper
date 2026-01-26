b7e92fb1b410267c97e86173651b4f5bc13a7405
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-01-27 07:30:42 +0800

    Update index.html and script.js for improved functionality and content

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const description =
         , `Your piece of paper is now 1.6mm thick, the thickness of a penny.`
         , `Your piece of paper is now 3.2mm thick, the size of a small fruitfly.`
         , `Your piece of paper is now 6.4mm thick, the width a of a small pea.`
+        , `Your piece of paper is now 12.8mm thick, the width of a keycap.`
     ]
 //Image srcs (accessed in update function)
 const imageSource =
@@ -25,6 +26,7 @@ const imageSource =
         , `images/penny.png`
         , `images/fruit-fly.png`
         , `images/pea.png`
+        , `images/keycap.png`
     ]
 //Image alts (accessed in update function)
 const imageAlt = 
@@ -36,9 +38,16 @@ const imageAlt =
         , `A penny.`
         , `A small fruitfly.`
         , `A pea.`
+        , `A keyboard.`
     ]
 //Fold limit variable
 let foldLimit = description.length - 1
+//Assigning button ids to variuables for event listeners.
+const fold = document.getElementById("fold")
+const unfold = document.getElementById("unfold")
+//Event listeners
+fold.addEventListener("click", fold)
+unfold.addEventListener("click", fold)
 //Generating ui
 image.src = imageSource[0]
 document.getElementById(`description-text`).textContent = description[0]
